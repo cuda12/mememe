b1c4734ecd91a4c043235aeedd84c3d8a8ac3afc
andy <arueesch@gmail.com>
2017-02-11 22:59:32 +0100

    feat: add collection view controller and collection view cell (cells not yet selectable, button missing)

---
+++ MemeMe/MemeCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Andreas Rueesch on 11.02.17.
+//  Copyright © 2017 Andreas Rueesch. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: Members
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
---
+++ MemeMe/MemeCollectionViewController.swift
@@ -0,0 +1,87 @@
+//
+//  MemeCollectionViewController.swift
+//  MemeMe
+//
+//  Created by Andreas Rueesch on 11.02.17.
+//  Copyright © 2017 Andreas Rueesch. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class MemeCollectionViewController: UICollectionViewController {
+
+    // MARK: Members
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    @IBOutlet var memeCollectionView: UICollectionView!
+    
+    // MARK: View Controller methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // reload data
+        memeCollectionView.reloadData()
+    }
+    
+    // MARK: UICollectionViewDataSource
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        print(appDelegate.memes.count)
+        return appDelegate.memes.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MemeCollectionViewCell", for: indexPath) as! MemeCollectionViewCell
+        let meme = appDelegate.memes[(indexPath as NSIndexPath).row]
+        
+        cell.imageView.image = meme.memedImage
+
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
